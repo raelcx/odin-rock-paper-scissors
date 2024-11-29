67bad49eea12a71d3a78e624504009806d89c86d
Israel Carvalho <77789295+raelcx@users.noreply.github.com>
2024-11-28 21:19:15 -0300

    Fix 'getComputerChoise' logic Create 'playGame' function

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function getComputerChoice() {
-    let randomNumber;
     let generateRandomNumber = Math.floor((Math.random() * 3) + 1 )
+    let computerChoice;
 
     switch(generateRandomNumber) {
-        case(1): randomNumber = "rock";
+        case(1): computerChoice = "rock";
         break
-        case(2): randomNumber = "paper";
+        case(2): computerChoice = "paper";
         break
-        case(3): randomNumber = "scissors";
+        case(3): computerChoice = "scissors";
         break
     }
 
-    return generateRandomNumber;
+    return computerChoice;
 }
 
 function getHumanChoice() {
@@ -34,31 +34,44 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == computerChoice) {
         console.log("It's a tie!");
-        return;
+        return 0;
     }
     
     if (humanChoice == "rock" && computerChoice == "scissors") {
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        return;
+        return 0;
     } 
 
     if (humanChoice == "paper" && computerChoice == "rock") {
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        return;
+        return 0;
     } 
 
     if (humanChoice == "scissors" && computerChoice == "paper") {
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        return;
+        return 0;
     } 
     
     else {
         console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        return;
+        return 1;
     }
+}
+
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    let result = playRound(getHumanChoice(), getComputerChoice());
+
+    if (result == 0) {
+        humanScore += 1;
+    } else {
+        computerScore += 1;
+    }
+
 }
 
 
 
-let humanScore = 0;
-let computerScore = 0;
